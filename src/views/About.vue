a77f879b994c8711923420f14a2c9f52962313f5
kelsodumez <56610196+kelsodumez@users.noreply.github.com>
2023-06-15 21:59:27 +1200

    PSUSH!!!

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,12 +6,31 @@
       <div>
         I'm <strong>Kelso du Mez</strong>, a second year game design student at the University of Canterbury.<br/>
 
-        Game Development is a strong passion of mine, with most of my expertise lying in gameplay programming in Unity
+        Game Development is a strong passion of mine, with most of my expertise lying in gameplay programming in Unity, I also have experience working in VR.
+        <div>In the next 5 years I aspire to create a complete and moderately successful indy-game.</div>
+
       </div>
 
       <!-- <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">games</router-link></div> on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div> -->
 
-      <div style="margin-top: 40px;">I'm <strong>currently looking for work experience</strong>, specifically programming positions, but im open to any and all offers. You can reach me at <a href="mailto:kelsodumez@gmail.com">kelsodumez@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I'm <strong>currently looking for work experience</strong>, specifically programming positions, but am open to any and all offers. You can reach me at <a href="mailto:kelsodumez@gmail.com">kelsodumez@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
+   
+      <div style="margin-top: 40px;">
+        I am currently working on my ideal VR game, It will be a roguelite FPS set in a post-apocalyptic shopping mall. In this game the player will have to drag their way through different stores whilst taking out various strange enemies.
+      </div>
+
+      <div style="margin-top: 20px;">
+        The game will have a unique mechanic where the players legs do not function and they must drag themselves across the floor with their controllers whilst shooting any enemies they encounter.
+      </div>
+      <div style="margin-top: 20px;">
+        I want to create a feeling of built up tension and suspense in the player.
+      </div>
+      <div style="margin-top: 20px;">
+        This will be developed so that it works for as many VR devices as possible in order to maximise its player-base.
+      </div>
+      <div style="margin-top: 20px;">
+        I will be using the agile framework in order to create a smooth development cycle
+      </div>
     </div>
 
     <div class="photo">
